3d1468733d0f56240cccdccdf7d7088d7afd0727
Shaga Sresthaa <shagasresthaa@gmail.com>
2024-06-17 14:32:30 -0400

    ui fixes, page title dynamic updates implemented, responsiveness improvements, code refactor

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -21,19 +21,19 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
+          <li class="nav-item" @click="closeMenu">
             <router-link class="nav-link" to="/">Home</router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item" @click="closeMenu">
             <router-link class="nav-link" to="/about">About</router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item" @click="closeMenu">
             <router-link class="nav-link" to="/projects">Projects</router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item" @click="closeMenu">
             <router-link class="nav-link" to="/blog">Blog</router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item" @click="closeMenu">
             <router-link class="nav-link" to="/contact">Contact</router-link>
           </li>
         </ul>
@@ -64,6 +64,12 @@ export default defineComponent({
   methods: {
     onLogin() {
       console.log("Login clicked");
+    },
+    closeMenu() {
+      const navbar = document.getElementById("navbarNav");
+      if (navbar && navbar.classList.contains("show")) {
+        navbar.classList.remove("show");
+      }
     },
   },
 });
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,41 +1,53 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomePage from "../views/HomePage.vue";
-import AboutPage from "../views/AboutPage.vue";
-import ProjectsPage from "../views/ProjectsPage.vue";
-import BlogPage from "../views/BlogPage.vue";
-import ContactPage from "../views/ContactPage.vue";
+import HomePage from "@/views/HomePage.vue";
+import AboutPage from "@/views/AboutPage.vue";
+import ProjectsPage from "@/views/ProjectsPage.vue";
+import BlogPage from "@/views/BlogPage.vue";
+import ContactPage from "@/views/ContactPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: HomePage,
+    meta: { title: "Home" },
   },
   {
     path: "/about",
     name: "About",
     component: AboutPage,
+    meta: { title: "About" },
   },
   {
     path: "/projects",
     name: "Projects",
     component: ProjectsPage,
+    meta: { title: "Projects" },
   },
   {
     path: "/blog",
     name: "Blog",
     component: BlogPage,
+    meta: { title: "Blog" },
   },
   {
     path: "/contact",
     name: "Contact",
     component: ContactPage,
+    meta: { title: "Contact" },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title as string;
+  }
+  next();
 });
 
 export default router;
