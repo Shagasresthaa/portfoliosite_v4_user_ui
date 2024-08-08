e67cb4a4700f5b538cfc08eacf7e1b34b951ea7f
Shaga Sresthaa <shagasresthaa@gmail.com>
2024-08-08 16:05:29 -0400

    added starter about page content

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="about"></div>
+  <div class="about">
+    <div class="content-area">
+      <div class="quest-area">
+        <img src="@/assets/about.jpg" alt="" class="propic" />
+        <p class="quest">
+          <b
+            >What happens when a software engineer, driven by a relentless
+            curiosity, begins to look beyond the code and turns their gaze
+            towards the stars and the vast expanse of the cosmos?</b
+          >
+        </p>
+      </div>
+      <p class="cont">
+        &nbsp;&nbsp;&nbsp;&nbsp;That’s the journey I’m on—a master’s student in
+        computer science at Western Michigan University, where my work in
+        full-stack development intersects with a deep fascination for the
+        universe. I spend my days focused on software development and
+        problem-solving, while my free moments are dedicated to exploring
+        astrophysics, particularly the signals of pulsars and the dynamics of
+        galaxies.<br />
+
+        &nbsp;&nbsp;&nbsp;&nbsp;My path has always been driven by more than just
+        a desire to build and solve—it’s fueled by relentless curiosity and a
+        passion for learning, not just individually, but alongside others who
+        share similar interests. Whether I’m tackling a coding challenge or
+        delving into radio astronomy, it’s the shared journey of exploration and
+        growth that truly excites me.<br />
+
+        &nbsp;&nbsp;&nbsp;&nbsp;I’m drawn to environments where passion and
+        curiosity lead the way, and where the process of learning and growing
+        together is what matters most. If you’re intrigued by the blend of
+        technology and astronomy, or if you share a passion for exploring the
+        unknown, let’s connect. There’s a universe of ideas to explore, and I’m
+        always eager to see where the journey might lead us.
+      </p>
+    </div>
+  </div>
   <FooterComponent />
 </template>
 
